bf6a811f1cd123ac5e34761b7fde6c86a432e295
varOnevar <2198093767@qq.com>
2022-08-17 23:53:09 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
     app
+     <img id="img" crossorigin="*" src="./baidu.png" />
   </div>
 </template>
 
 <script>
-import Print, { getImage } from '../lib/index'
+import Print, { getImage, getDomImage } from '../lib/index'
 import image from './baidu.png'
 export default {
   name: 'App',
-  async created() {
+  async mounted() {
     console.log(image, '??')
     const printer = new Print()
     const img = await getImage(image)
     console.log(printer, 'print', img)
     printer.align('ct')
-       .image(img, 's8')
-       .then(() => { 
-          printer.cut()
-       });
+      .image(img, 's8')
+      .then(() => { 
+        printer.cut()
+        console.log(printer.getBuffer(), 111)
+      });
+      this.$nextTick(() => {
+        const imgssss = document.querySelector('#img')
+        const { width ,height } = imgssss.getBoundingClientRect()
+        console.log(width ,height)
+        const scrw = 200
+        let scrh = Math.ceil(height * scrw / width)
+        scrh = scrh + 8 - (scrh % 8)
+        console.log(scrw ,scrh, 222)
+        const zzz = getDomImage(imgssss, scrw, scrh)
+        console.log(zzz, 222)
+      })
   }
 }
 </script>
